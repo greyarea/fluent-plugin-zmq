d7e97df057d487969bc2b5b8b49a71d7658b1858
Teemu Tuulari <teemu.tuulari@greyarealabs.com>
2013-01-31 12:30:09 +0200

    No msgpack usage. Split by two tabs

--- lib/fluent/plugin/in_zmq.rb
+++ lib/fluent/plugin/in_zmq.rb
@@ -64,7 +64,7 @@ class ZMQInput < Input
         ret = ZMQ::select([@server])
         ret[0].each do |sock|
           msg = sock.recv
-          on_message(MessagePack.unpack(msg))
+          on_message(msg.split("\t", 3))
         end
       end
     rescue
@@ -96,33 +96,10 @@ class ZMQInput < Input
   def on_message(msg)
     # TODO format error
     tag = msg[0].to_s
-    entries = msg[1]
-
-    if entries.class == String
-      # PackedForward
-      es = MessagePackEventStream.new(entries, @cached_unpacker)
-      Engine.emit_stream(tag, es) 
-
-    elsif entries.class == Array
-      # Forward
-      es = MultiEventStream.new
-      entries.each {|e|
-        time = e[0].to_i
-        time = (now ||= Engine.now) if time == 0
-        record = e[1]
-        es.add(time, record)
-      }
-      Engine.emit_stream(tag, es)
-    else
-      # Message
-      time = msg[1]
-      time = Engine.now if time == 0
-      record = msg[2]
-      Engine.emit(tag, time, record)
-    end
-    #p tag
-    #p time
-    #p record
+    time = Integer(msg[1])
+    time = Engine.now if time == 0
+    record = msg[2]
+    Engine.emit(tag, time, record)
   end
 
 end
